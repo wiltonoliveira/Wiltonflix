dcfec8456327a412de89d07b7f012e740d3426c0
Wilton Silva Oliveira Júnior <wiltonjr@iMac-Pro.local>
2020-07-28 15:46:47 -0300

    Roteamento entre as páginas via SPA

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import {  BrowserRouter, Switch, Route} from 'react-router-dom'; 
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (<div><h1>Erro 404</h1></div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
+  // <React.StrictMode>
+  //   <Home />
+  // </React.StrictMode>,
 );
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
+
+function CadastroVideo(){
+    return (
+      <PageDefault>
+        <h1>Cadastro de Vídeo</h1>
+
+        <Link to="/cadastro/categoria">
+            Cadastro Catégoria
+        </Link>
+      </PageDefault>
+     )
+  }
+
+  export default  CadastroVideo;
